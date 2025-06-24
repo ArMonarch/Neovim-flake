79138429e27ce2784afb369d7f69568e875c917e
ArMonarch <praffulthapa12@gmail.com>
2025-06-24 21:10:26 +0545

    fix: Add LSP & Plugins before keymaps and autocmd as some keymaps depends upon plugins

--- lua/frenzfries/init.lua
+++ lua/frenzfries/init.lua
@@ -1,14 +1,16 @@
 -- load set vim options
 require("frenzfries.options")
 
--- load set vim keymaps
-require("frenzfries.keymaps")
-
--- load set vim autocmds
-require("frenzfries.autocmds")
-
+-- NOTE: Add LSP & Plugins before keymaps and autocmds as some
+-- of the keymaps and autocmd depend upon these plugins
 -- NOTE: module for configuring LSPs
 require('frenzfries.lsp')
 
 -- load plugins
 require("frenzfries.plugins")
+
+-- load set vim keymaps
+require("frenzfries.keymaps")
+
+-- load set vim autocmds
+require("frenzfries.autocmds")
