ab504bf58cd30101b909dfec8e75c3c5d0cbf983
ArMonarch <praffulthapa12@gmail.com>
2025-06-24 21:09:25 +0545

    fix: typo filetype -> filetypes
    This seems to activate the pyright language server of every file type
    resulting in pyright always starting for every file, causing conflicting
    behavoiurs across the lsp setting

--- lua/frenzfries/lsp/config/pyright.lua
+++ lua/frenzfries/lsp/config/pyright.lua
@@ -52,16 +52,16 @@
 --   end,
 -- })
 
--- vim.lsp.config('pyright', {
---   cmd = { 'pyright-langserver', '--stdio'},
---   filetype = { 'python' },
---   root_markers = {
---     'pyproject.toml',
---     'setup.py',
---     'setup.cfg',
---     'requirements.txt',
---     'Pipfile',
---     'pyrightconfig.json',
---     '.git',
---   },
--- })
+vim.lsp.config('pyright', {
+  cmd = { 'pyright-langserver', '--stdio'},
+  filetypes = { 'python' },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    'pyrightconfig.json',
+    '.git',
+  },
+})
