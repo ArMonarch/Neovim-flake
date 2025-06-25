0001e9df0113c2a757bd1f31b72c7b88140535c7
ArMonarch <praffulthapa12@gmail.com>
2025-06-25 13:56:09 +0545

    feat: python lsp setup with organize imports setting

--- lua/frenzfries/lsp/config/pyright.lua
+++ lua/frenzfries/lsp/config/pyright.lua
@@ -56,17 +56,3 @@ vim.lsp.config('pyright', {
     })
   end,
 })
-
--- vim.lsp.config('pyright', {
---   cmd = { 'pyright-langserver', '--stdio'},
---   filetypes = { 'python' },
---   root_markers = {
---     'pyproject.toml',
---     'setup.py',
---     'setup.cfg',
---     'requirements.txt',
---     'Pipfile',
---     'pyrightconfig.json',
---     '.git',
---   },
--- })
