5e4e7c021e187737cbe491053ca9e1a53d29073c
ArMonarch <praffulthapa12@gmail.com>
2025-06-25 13:55:10 +0545

    feat: updated lua_ls settings

--- lua/frenzfries/lsp/config/lua_ls.lua
+++ lua/frenzfries/lsp/config/lua_ls.lua
@@ -1,40 +1,8 @@
+-- TODO: Lua Language Server Configuration setup needed?
+-- lazydev already setups the lua-language-server ready for lua development,
+-- is the extra Configuration as from nvim-lsp for lua needed?
+
 vim.lsp.config('lua_ls', {
-  -- on_init = function(client)
-  --   if client.workspace_folders then
-  --     local path = client.workspace_folders[1].name
-  --     if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
-  --       return
-  --     end
-  --   end
-  --
-  --   client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-  --     runtime = {
-  --       -- Tell the language server which version of Lua you're using (most
-  --       -- likely LuaJIT in the case of Neovim)
-  --       version = 'LuaJIT',
-  --       -- Tell the language server how to find Lua modules same way as Neovim
-  --       -- (see `:h lua-module-load`)
-  --       path = {
-  --         'lua/?.lua',
-  --         'lua/init.lua',
-  --       },
-  --     },
-  --     -- Make the server aware of Neovim runtime files
-  --     workspace = {
-  --       checkThirdParty = false,
-  --       library = {
-  --         vim.env.VIMRUNTIME
-  --         -- Depending on the usage, you might want to add additional paths
-  --         -- here.
-  --         -- '${3rd}/luv/library'
-  --         -- '${3rd}/busted/library'
-  --       },
-  --     },
-  --   })
-  -- end,
-  -- settings = {
-  --   Lua = {}
-  -- },
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
   root_markers = {
