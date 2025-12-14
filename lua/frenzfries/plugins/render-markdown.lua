bf971855aa8d2f40ae345f0e95d0c97171ea425e
ArMonarch <praffulthapa12@gmail.com>
2025-12-14 19:47:11 +0545

    feat: removed markdown heading rendering

--- lua/frenzfries/plugins/render-markdown.lua
+++ lua/frenzfries/plugins/render-markdown.lua
@@ -1,12 +1,15 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  lazy = true, -- Recommended false
-  ft = { 'markdown', 'quarto' },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "echasnovski/mini.icons",
-  },
-  opts = {
-    completions = { blink = { enabled = true } },
-  },
+	"MeanderingProgrammer/render-markdown.nvim",
+	lazy = true, -- Recommended false
+	ft = { "markdown", "quarto" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"echasnovski/mini.icons",
+	},
+	opts = {
+		completions = { blink = { enabled = true } },
+		heading = {
+			enabled = false,
+		},
+	},
 }
